4c89d5fe73459964084fe22fdea63a02bdca65ce
luscagodoi <luscagodoi@gmail.com>
2025-08-25 16:56:30 -0300

    Create relatorio02_Calculadora.lua

--- Relatorio_02/relatorio02_Maior.lua
+++ Relatorio_02/relatorio02_Maior.lua
@@ -1,8 +1,8 @@
 local function maior_do_array (array)
   local maior = 0;
-  for i = 1, #array do
-    if array[i]>maior then
-      maior = array[i];
+  for _, numero in ipairs(array) do
+    if numero>maior then
+      maior = numero;
     end
   end
   return maior;
@@ -11,3 +11,7 @@ end
 local array = {1, -6, 22, 9, 3, 5, 2, 5, 15};
 
 print(maior_do_array(array));
+
+--Crie um programa que receba uma tabela (array) de números e retorne o maior valor encontrado nela.
+--A função deve percorrer todos os elementos da tabela.
+--O valor retornado deve ser apenas o maior número da tabela.
